28f0eb9f71a4b40bc054f002a65f9feac9c66f80
Giovanni Alfredo Garciliano Díaz <giobeatle10@gmail.com>
2016-11-10 21:18:25 -0600

    Upload intial version of dislines.php

---
+++ dislines.php
@@ -0,0 +1,248 @@
+<?php
+	/* Licensed under GNU Affero General Public License v3 */
+	/* Declare global variables (ISO ) */
+		$iso639[0] = "aa"; /* afar */
+		$iso639[1] = "ab"; /* abjasio */
+		$iso639[2] = "ae"; /* avéstico */
+		$iso639[3] = "af"; /* afrikáans */
+		$iso639[4] = "ak"; /* akano */
+		$iso639[5] = "am"; /* amhárico */
+		$iso639[6] = "an"; /* aragonés */
+		$iso639[7] = "ar"; /* árabe*/
+		$iso639[8] = "as"; /* asanés */
+		$iso639[9] = "av"; /* avar */
+		$iso639[10] = "ai"; /* aimara */
+		$iso639[11] = "az"; /* azerí */
+		$iso639[12] = "ba"; /* baskir */
+		$iso639[13] = "be"; /* bielorruso */
+		$iso639[14] = "bg"; /* búlgaro */
+		$iso639[15] = "bh"; /* bhoyapurí */
+		$iso639[16] = "bi"; /* bislama */
+		$iso639[17] = "bm"; /* bambara */
+		$iso639[18] = "bn"; /* bengalí */
+		$iso639[19] = "bo"; /* tibetano */
+		$iso639[20] = "br"; /* bretón */
+		$iso639[21] = "bs"; /* bosnio */
+		$iso639[22] = "ca"; /* catalán */
+		$iso639[23] = "ce"; /* checheno */
+		$iso639[24] = "ch"; /* chamorro */
+		$iso639[25] = "co"; /* corso */
+		$iso639[26] = "cr"; /* cree */
+		$iso639[27] = "cs"; /* checo */
+		$iso639[28] = "cu"; /* eslavo eclesiástico antiguo */
+		$iso639[29] = "cv"; /* chuvasio */
+		$iso639[30] = "cy"; /* galés */
+		$iso639[31] = "da"; /* danes */
+		$iso639[32] = "de"; /* alemán */
+		$iso639[33] = "dv"; /* maldivo */
+		$iso639[34] = "dz"; /* dzongkha */
+		$iso639[35] = "ee"; /* ewé */
+		$iso639[36] = "el"; /* griego moderno */
+		$iso639[37] = "en"; /* inglés */
+		$iso639[38] = "eo"; /* esperanto */
+		$iso639[39] = "es"; /* español */
+		$iso639[40] = "et"; /* estonio */
+		$iso639[41] = "eu"; /* euskera */
+		$iso639[42] = "fa"; /* persa */
+		$iso639[43] = "ff"; /* fula */
+		$iso639[44] = "fi"; /* finés */
+		$iso639[45] = "fj"; /* fiyiano */
+		$iso639[46] = "fo"; /* feroés */
+		$iso639[47] = "fr"; /* francés */
+		$iso639[48] = "fy"; /* frisón */
+		$iso639[49] = "ga"; /* irlandés */
+		$iso639[50] = "gd"; /* gaélico */
+		$iso639[51] = "gl"; /* gallego */
+		$iso639[52] = "gn"; /* guaraní */
+		$iso639[53] = "gu"; /* guyaratí */
+		$iso639[54] = "gv"; /* manés */
+		$iso639[55] = "ha"; /* hausa */
+		$iso639[56] = "he"; /* hebreo */
+		$iso639[57] = "hi"; /* hindi */
+		$iso639[58] = "ho"; /* hiri motu */
+		$iso639[59] = "hr"; /* croata */
+		$iso639[60] = "ht"; /* haitiano */
+		$iso639[61] = "hu"; /* húngaro */
+		$iso639[62] = "hy"; /* armenio */
+		$iso639[63] = "hz"; /* herero */
+		$iso639[64] = "ia"; /* interlingua */
+		$iso639[65] = "id"; /* indonesio */
+		$iso639[66] = "ie"; /* occidental */
+		$iso639[67] = "ig"; /* igbo */
+		$iso639[68] = "ii"; /* yi de Sichuán */
+		$iso639[69] = "ik"; /* iñupiaq */
+		$iso639[70] = "io"; /* ido */
+		$iso639[71] = "is"; /* islandés */
+		$iso639[72] = "it"; /* italiano */
+		$iso639[73] = "iu"; /* inuktitut*/
+		$iso639[74] = "ja"; /* japonés */
+		$iso639[75] = "jv"; /* javanés */
+		$iso639[76] = "ka"; /* georgiano */
+		$iso639[77] = "kg"; /* kongo */
+		$iso639[78] = "ki"; /* kikuyu */
+		$iso639[79] = "kj"; /* kuanyama */
+		$iso639[80] = "kk"; /* kazajo */
+		$iso639[81] = "kl"; /* groelandés */
+		$iso639[82] = "km"; /* camboyano */
+		$iso639[83] = "kn"; /* canarés */
+		$iso639[84] = "ko"; /* coreano */
+		$iso639[85] = "kr"; /* kanuri */
+		$iso639[86] = "ks"; /* cachemiro */
+		$iso639[87] = "ku"; /* kurdo */
+		$iso639[88] = "kv"; /* komi */
+		$iso639[89] = "kw"; /* córmico */
+		$iso639[90] = "ky"; /* kirguís */
+		$iso639[91] = "la"; /* latín */
+		$iso639[92] = "lb"; /* luxemburgués */
+		$iso639[93] = "lg"; /* luganda */
+		$iso639[94] = "li"; /* limburgués */
+		$iso639[95] = "ln"; /* lingala */
+		$iso639[96] = "lo"; /* lao */
+		$iso639[97] = "lt"; /* lituano */
+		$iso639[98] = "lu"; /* luba-katanga */
+		$iso639[99] = "lv"; /* letón */
+		$iso639[100] = "mg"; /* malgache */
+		$iso639[101] = "mh"; /* marshalés */
+		$iso639[102] = "mi"; /* maorí */
+		$iso639[103] = "mk"; /* macedonio */
+		$iso639[104] = "ml"; /* malayalam */
+		$iso639[105] = "mn"; /* mongol */
+		$iso639[106] = "mr"; /* maratí */
+		$iso639[107] = "ms"; /* malayo */
+		$iso639[108] = "mt"; /* maltés */
+		$iso639[109] = "my"; /* birmano */
+		$iso639[110] = "na"; /* nauruano */
+		$iso639[111] = "nb"; /* noruego bokmål */
+		$iso639[112] = "nd"; /* ndebele del norte */
+		$iso639[113] = "ne"; /* nepalí */
+		$iso639[114] = "ng"; /* ndonga */
+		$iso639[115] = "nl"; /* neerlandés */
+		$iso639[116] = "nn"; /* nynorsk */
+		$iso639[117] = "no"; /* noruego */
+		$iso639[118] = "nr"; /* ndelebe del sur */
+		$iso639[119] = "nv"; /* navajo */
+		$iso639[120] = "ny"; /* chichewa */
+		$iso639[121] = "oc"; /* occitano */
+		$iso639[122] = "oj"; /* ojibwa */
+		$iso639[123] = "om"; /* oromo */
+		$iso639[124] = "or"; /* oriya */
+		$iso639[125] = "os"; /* osético */
+		$iso639[126] = "pa"; /* panyabí */
+		$iso639[127] = "pi"; /* pali */
+		$iso639[128] = "pl"; /* polaco */
+		$iso639[129] = "ps"; /* pastú */
+		$iso639[130] = "pt"; /* portugués */
+		$iso639[131] = "qu"; /* quechua */
+		$iso639[132] = "rm"; /* romanche */
+		$iso639[133] = "rn"; /* kirundi */
+		$iso639[134] = "ro"; /* rumano */
+		$iso639[135] = "ru"; /* ruso */
+		$iso639[136] = "rw"; /* ruandés */
+		$iso639[137] = "sa"; /* sánscrito */
+		$iso639[138] = "sc"; /* sardo */
+		$iso639[139] = "sd"; /* sindhi */
+		$iso639[140] = "se"; /* sami septentrional */
+		$iso639[141] = "sg"; /* sango */
+		$iso639[142] = "si"; /* cingalés */
+		$iso639[143] = "sk"; /* eslovaco */
+		$iso639[144] = "sl"; /* eslovenio */
+		$iso639[145] = "sm"; /* samoano */
+		$iso639[146] = "sn"; /* shona */
+		$iso639[147] = "so"; /* somalí */
+		$iso639[148] = "sq"; /* albanés */
+		$iso639[149] = "sr"; /* serbio */
+		$iso639[150] = "ss"; /* suazi */
+		$iso639[151] = "st"; /* seshoto */
+		$iso639[152] = "su"; /* sundanés */
+		$iso639[153] = "sv"; /* sueco */
+		$iso639[154] = "sw"; /* suajili */
+		$iso639[155] = "ta"; /* tamil */
+		$iso639[156] = "te"; /* tégulu */
+		$iso639[157] = "tg"; /* tayiko */
+		$iso639[158] = "th"; /* tailandés */
+		$iso639[159] = "ti"; /* tigriña */
+		$iso639[160] = "tk"; /* turcomano */
+		$iso639[161] = "tl"; /* tagalo */
+		$iso639[162] = "tn"; /* setsuana */
+		$iso639[163] = "to"; /* tongano */
+		$iso639[164] = "tr"; /* turco */
+		$iso639[165] = "ts"; /* tsonga */
+		$iso639[166] = "tt"; /* tártaro */
+		$iso639[167] = "tw"; /* twi */
+		$iso639[168] = "ty"; /* tahitiano */
+		$iso639[169] = "ug"; /* uigur */
+		$iso639[170] = "uk"; /* ucraniano */
+		$iso639[171] = "ur"; /* urdu */
+		$iso639[172] = "uz"; /* uzbeko */
+		$iso639[173] = "ve"; /* venda */
+		$iso639[174] = "vi"; /* vietnamita */
+		$iso639[175] = "vo"; /* volapük */
+		$iso639[176] = "wa"; /* valón */
+		$iso639[177] = "wo"; /* wolof */
+		$iso639[178] = "xh"; /* xhosa */
+		$iso639[179] = "yi"; /* yiddish */
+		$iso639[180] = "yo"; /* yoruba */
+		$iso639[181] = "za"; /* chuan */
+		$iso639[182] = "zh"; /* chino */
+		$iso639[183] = "zu"; /* zulú */
+	function infoprog(){
+		$versionarray = array(
+			"version" => "1.0",
+			"versiondislines" => "2.0",
+			"lang" => "PHP",
+			"author" => "Giovanni Alfredo Garciliano Diaz",
+			"basedon" => "Based on a work of Daniel Clemente Laboreo (http://danielclemente.com/dislines)",
+			"license" => "GNU Affero General Public License v3",
+			"url" => "http://giobeatle1794.esy.es/dislines",
+			"createdusing" => "gedit, Firefox, FileZilla",
+		);
+		return $versionarray;
+	}
+	function parser($filename,$lang){
+		$counter = 0; 
+		$file = fopen($filename, "r");
+		while(!feof($file)) {
+			$line[$counter] = fgets($file);
+			$counter++;
+		}
+		fclose($file);
+		/* The parsing process begins here */
+		$tag = "@".$lang."{";
+		$endtag = "@}";
+		$lines = count($line) - 1;
+		$iminsidetag = false;
+		$i = 0;
+		while ($i <= $lines) {
+			if ($iminsidetag == true) {
+				$tagtoanalyze2 = substr($line[$i], 0, 2);
+				while ($tagtoanalyze2 != $endtag){
+					echo $line[$i];
+					$i++;
+					$tagtoanalyze2 = substr($line[$i], 0, 2);
+				}
+				$iminsidetag = false;
+			} else {
+				$tagtoanalyze = substr($line[$i], 0, 4);
+				if ($tagtoanalyze == $tag){
+					$iminsidetag = true;
+				}
+			}
+			$i++;
+		}
+	}
+	function main($filename,$lang){
+		global $iso639;
+		$filename = "test.dsl";
+		$lang = "es";
+		$countiso = 0;
+		while ($countiso <= 183){
+			if ($lang == $iso639[$countiso]){
+				parser($filename,$lang);
+				return 0;
+			} else {
+				$countiso++;
+			}
+		}
+		return 1; /* Error 1: Language must be any specified on ISO 639-1 */
+	}
+?>
